10b4adc7e15a4bafff662ef07f23defc0ff01c03
Wuletaw <wuletawwonte@yahooo.com>
2019-01-05 20:06:58 +0300

    Login Page Setup Finished

---
+++ application/views/login.php
@@ -0,0 +1,136 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+		<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="description" content="Neon Admin Panel" />
+	<meta name="author" content="" />
+	
+	<title>login | PencilCrunch School</title>
+	
+
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/js/jquery-ui-1.10.3.custom.min.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/font-icons/entypo/<?php echo base_url(); ?>assets/css/entypo.css">
+	<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/neon-core.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/neon-theme.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/neon-forms.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/custom.css">
+
+	<script src="<?php echo base_url(); ?>assets/js/jquery-1.11.0.min.js"></script>
+
+	<link rel="shortcut icon" href="http://testlimat.wuletaw/assets/images/favicon.png">
+	
+</head>
+<body class="page-body login-page login-form-fall" data-url="http://neon.dev">
+
+
+<script type="text/javascript">
+var baseurl = 'http://testlimat.wuletaw/';
+</script>
+
+<div class="login-container">
+	
+	<div class="login-header login-caret">
+		
+		<div class="login-content" style="width:100%;">
+			
+			<a href="<?php echo base_url(); ?>" class="logo">
+				<img src="<?php echo base_url(); ?>assets/img/logo.png" height="80" alt="" />			</a>
+			
+			<p class="description">
+            	<h2 style="color:#cacaca; font-weight:100;">
+					የአርባምንጭ መካነ እየሱስ ምዕመናን መረጃ ቋት              </h2>
+           </p>
+			
+			<!-- progress bar indicator -->
+			<div class="login-progressbar-indicator">
+				<h3>43%</h3>
+				<span>logging in...</span>
+			</div>
+		</div>
+		
+	</div>
+	
+	<div class="login-progressbar">
+		<div></div>
+	</div>
+	
+	<div class="login-form">
+		
+		<div class="login-content">
+			
+			<div class="form-login-error">
+				<h3>Invalid login</h3>
+				<p>Please enter correct email and password!</p>
+			</div>
+			
+			<form method="post" role="form" id="form_login">
+				
+				<div class="form-group">
+					
+					<div class="input-group">
+						<div class="input-group-addon">
+							<i class="entypo-user"></i>
+						</div>
+						
+						<input type="text" class="form-control" name="email" id="email" placeholder="ስም" autocomplete="off" data-mask="email" />
+					</div>
+					
+				</div>
+				
+				<div class="form-group">
+					
+					<div class="input-group">
+						<div class="input-group-addon">
+							<i class="entypo-key"></i>
+						</div>
+						
+						<input type="password" class="form-control" name="password" id="password" placeholder="የይለፍ ቃል" autocomplete="off" />
+					</div>
+				
+				</div>
+				
+				<div class="form-group">
+					<button type="submit" class="btn btn-primary btn-block btn-login">
+						<i class="entypo-login"></i>
+						ይግቡ
+					</button>
+				</div>
+				
+						
+			</form>
+			
+			
+			<div class="login-bottom-links">
+				<a href="http://testlimat.wuletaw/login/forgot_password" class="link">
+					የይለፍ ቃል ረሱ ?
+				</a>
+			</div>
+			
+		</div>
+		
+	</div>
+	
+</div>
+
+
+	<!-- Bottom Scripts -->
+	<script src="<?php echo base_url(); ?>assets/js/gsap/main-gsap.js"></script>
+	<script src="<?php echo base_url(); ?>assets/js/jquery-ui/<?php echo base_url(); ?>assets/js/jquery-ui-1.10.3.minimal.min.js"></script>
+	<script src="<?php echo base_url(); ?>assets/js/bootstrap.js"></script>
+	<script src="<?php echo base_url(); ?>assets/js/joinable.js"></script>
+	<script src="<?php echo base_url(); ?>assets/js/resizeable.js"></script>
+	<script src="<?php echo base_url(); ?>assets/js/neon-api.js"></script>
+	<script src="<?php echo base_url(); ?>assets/js/jquery.validate.min.js"></script>
+	<script src="<?php echo base_url(); ?>assets/js/neon-login.js"></script>
+	<script src="<?php echo base_url(); ?>assets/js/neon-custom.js"></script>
+	<script src="<?php echo base_url(); ?>assets/js/neon-demo.js"></script>
+
+</body>
+</html>
