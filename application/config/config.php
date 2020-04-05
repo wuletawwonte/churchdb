5c511ddcaf5f55f10eedc4e0718ed02541cee5dd
Wuletaw Wonte <wuletawwonte@yahoo.com>
2020-04-05 16:43:37 +0300

    set http to https

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = "http://".$_SERVER["HTTP_HOST"]."/";
+$config['base_url'] = "";
 
 /*
 |--------------------------------------------------------------------------
