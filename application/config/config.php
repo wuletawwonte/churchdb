dd6a8c75b2e60f58b1e595cb0341e20d6f783858
Wuletaw Wonte <wuletawwonte@yahoo.com>
2020-04-05 16:45:31 +0300

    base_url changed to the domain name

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = "";
+$config['base_url'] = "https://churchdb.amyc.org.et/";
 
 /*
 |--------------------------------------------------------------------------
